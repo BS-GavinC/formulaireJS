f813f8a7b2652f4c3a1b207a4bfe8368d8747ff4
BS-GavinC <98208007+VZGLY@users.noreply.github.com>
2023-06-20 16:44:09 +0200

    exo2

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ let autoComplete = () => {
 let reset = () => {
     formulaire.reset()
 }
+
+let valider = () => {
+    if (!formulaire.firstname.value) {
+        alert("prenom invalide !")
+    }
+    else if(!formulaire.lastname.value){
+        alert("nom de famille invalide !")
+    }
+    else if(!(formulaire.postalCode.value > 1000 && formulaire.postalCode.value < 9999)){
+        alert("Code postal invalide !")
+    }
+    else{
+        alert("Formulaire validé !")
+    }
+}
